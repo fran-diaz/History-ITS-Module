ce7f38c5d7b15eefe9bd6a2e833324095879ca70
Fran Díaz <fran.diaz.gonzalez@gmail.com>
2021-04-21 09:09:29 +0200

    Update history.php

--- history.php
+++ history.php
@@ -2,7 +2,6 @@
 /**
  * Componente text
  */
-
 class history extends base_component implements components_interface {
 
 	public function make_history() : string {
@@ -12,26 +11,27 @@ class history extends base_component implements components_interface {
 		?>
 		<div class="p-3">
 		<p class="small alert alert-warning">* En amarillo, el valor previo de los campos que han sido cambiados.</p>
+		<div class="accordion>
 		<?php
 		if( isset($_REQUEST['d']) ){
 			$data = decode( $_REQUEST['d'] );
 			$rows = $this -> _ITExt -> select('system__history','*', ['table' => $data['table'],'ref' => $data['id'],'ORDER' => ['date' => 'DESC']]);
 			$actual_data = $this -> _ITExt -> get($data['table'],'*', [ $data['table'].'_id' => $data['id'] ]);
-			echo '<div class="accordion" id="accordionExample">';
-			if($rows){foreach($rows as $num => $row){
+			if($rows){
+				foreach($rows as $num => $row) {
 				//$json_string = json_encode(json_decode($row['data']), JSON_PRETTY_PRINT); 
 				//echo "<h4 class=\"mt-3 mb-1\">".$_ITE->funcs->date_format($row['date'],5)."</h4><pre>".$json_string."</pre>";
 				?>
 				  <div class="card">
-				    <div class="card-header" id="headingOne">
+				    <div class="card-header" id="heading<?=$num?>">
 				      <h2 class="mb-0">
-				        <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+				        <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapse<?=$num?>" aria-expanded="true">
 				          <?=$this -> _ITE -> funcs -> date_format( $row['date'], 5 )?>
 				        </button>
 				      </h2>
 				    </div>
 
-				    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+				    <div id="collapse<?=$num?>" class="collapse">
 				      <div class="card-body">
 				      	<ul>
 				        <?php
@@ -51,12 +51,12 @@ class history extends base_component implements components_interface {
 				      </div>
 				    </div>
 				  </div>
-				  
-				
-			}}
-			echo '</div>';
+				<?php
+				}
+			}
 		}
 		?>
+		</div>
 		</div>
 		<?php
 
